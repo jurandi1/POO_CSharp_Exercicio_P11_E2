ad3e5eff4cf3e941dda200ebfe4ad32e86c5b0f5
Jurandi Junior <jurandijr20@gmail.com>
2025-01-21 20:52:19 -0300

    Exercício resolvido

---
+++ Entities/Comment.cs
@@ -0,0 +1,11 @@
+﻿namespace POO_CSharp_Exercicio_P11_E2.Entities
+{
+    internal class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment() { }
+
+        public Comment(string text) { this.Text = text; }
+    }
+}
